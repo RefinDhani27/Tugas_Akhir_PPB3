5e82e246f215264d9f9ff31796922410e7a12f39
Refin Dhani <RefinLegacy@gmail.com>
2023-11-27 18:32:02 +0700

    Initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,62 @@
-import { Text, View } from '../../components/Themed';
-import { ScrollView, TextInput, Image, StyleSheet} from 'react-native';
+import React, { useState, useRef } from 'react';
+import { Text, View, ScrollView, Image, StyleSheet, Dimensions, TouchableOpacity, NativeSyntheticEvent, NativeScrollEvent} from 'react-native';
 
 const logo = require('../../assets/images/logo.png');
+
+const carouselImages = [
+  require('../../assets/images/gambar_1.png'),
+  require('../../assets/images/gambar_1.png'),
+  require('../../assets/images/gambar_1.png'),
+];
+
+const { width } = Dimensions.get('window');
+
 export default function TabOneScreen() {
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const carouselRef = useRef<ScrollView>(null);
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const contentOffsetX = event.nativeEvent.contentOffset.x;
+    const newIndex = Math.round(contentOffsetX / width);
+    if (newIndex !== activeIndex) {
+      setActiveIndex(newIndex);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
-        <Image source={logo} style={{ height: 40}} resizeMode="contain" />
+        <View style={styles.carouselContent}>
+        <ScrollView
+          ref={carouselRef}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          onScroll={onScroll}
+          scrollEventThrottle={16}
+          style={styles.carouselContainer}
+        >
+          {carouselImages.map((image, index) => (
+            <Image key={index} source={image} style={styles.carouselImage} />
+          ))}
+        </ScrollView>
+        </View>
+        <View style={styles.paginationWrapper}>
+          {carouselImages.map((_, index) => (
+            <View
+              key={index}
+              style={[styles.dot, index === activeIndex ? styles.dotActive : styles.dotInactive]}
+            />
+          ))}
+        </View>
+        <Image source={logo} style={styles.logo} resizeMode="contain" />
         <Text style={styles.selamatDatang}>Selamat Datang di {'\n'}Recom Movie</Text>
       </View>
-      <View style={styles.putihBawah}>
-        
-      </View>
+      <TouchableOpacity style={styles.buttonMasukContainer}>
+          <Text style={styles.buttonMasuk}>MASUK</Text>
+      </TouchableOpacity>
+      <View style={styles.putihBawah} />
     </View>
   );
 }
@@ -22,6 +67,54 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#062148'
+  },
+  logo: {
+    height: 40
+  },
+  carouselContent: {
+    height: '52%',
+    width: '100%',
+  },
+  carouselContainer: {
+    height: '100%',
+    width: width,
+  },
+  carouselImage: {
+    width: width,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  paginationWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 30
+  },
+  dot: {
+    height: 8,
+    width: 8,
+    borderRadius: 4,
+    marginHorizontal: 4,
+    backgroundColor: '#3AB6D1',
+  },
+  dotActive: {
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+  },
+  dotInactive: {
+    
+  },
+  buttonMasukContainer: {
+    backgroundColor: "#FCA806",
+    borderRadius: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+    marginBottom: -20,
+    zIndex: 10
+  },
+  buttonMasuk: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: "#fff",
   },
   title: {
     fontSize: 20,
@@ -43,7 +136,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 180,
+    marginTop: 5,
     backgroundColor: '#062148'
   },
   selamatDatang: {
