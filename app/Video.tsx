43e1fa8dd3ac831b36b61c3648230d74fa1f53be
khamal45 <mahendra_21510020@stimata.ac.id>
2024-01-05 12:42:05 +0700

    semoga berkah

--- app/Video.tsx
+++ app/Video.tsx
@@ -1,16 +1,5 @@
-import { Video, ResizeMode } from 'expo-av';
-export default function Video(){
-return{
-    <Video
-        ref={video}
-        style={styles.video}
-        source={{
-          uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-        }}
-        useNativeControls
-        resizeMode={ResizeMode.CONTAIN}
-        isLooping
-        onPlaybackStatusUpdate={status => setStatus(() => status)}
-      />
-}
+import { Video, ResizeMode } from "expo-av";
+import { View } from "react-native";
+export default function Video() {
+  return <View />;
 }
